b0a1738dcacd2f74687e0d1ae4618b4ed9026c88
unknown <pedro.livi2005@gmail.com>
2026-03-16 17:25:41 -0300

    sexto commit

---
+++ aulas/aula020.php
@@ -0,0 +1,14 @@
+<?php
+	$nome = "Gustavo";
+	$sobrenome = "Guanabara";
+	const PAIS = "Brasil";
+	//
+	//
+	//
+	//
+	//const PAIS = "EUA";
+	//
+	//
+	//
+	//
+	echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS ;
