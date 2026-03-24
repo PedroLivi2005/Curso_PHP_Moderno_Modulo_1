be2ad830605e1270643d71b54d755fb480767468
unknown <pedro.livi2005@gmail.com>
2026-03-24 17:30:21 -0300

    decimo commit

---
+++ desafios/desafio001/resposta.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP 001 Resposta</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Resultado Final</h1>
+        <?php
+            $_numero = $_GET["numero"];
+    
+            $antecessor = $_numero - 1;
+            $sucessor = $_numero + 1;
+
+            echo "<p>O número escolhido foi <strong>$_numero</strong></p>";
+            echo "<p>O seu <i>antecessor</i> é $antecessor</p>";
+            echo "<p>O seu <i>sucessor</i> é $sucessor</p>";
+        ?>
+        <button>
+            <a href="javascript:history.go(-1)">← Voltar</a>
+        </button>
+    </main>
+</body>
+</html>
