ea43ba315842c9e9a741647acaa8054833e50f81
unknown <pedro.livi2005@gmail.com>
2026-04-08 17:29:13 -0300

    14-1

---
+++ desafios/desafio011/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP 011</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    $preco = (float) ($_GET["preco"] ?? 0);
+
+    $reajuste = $_GET["reajuste"] ?? 0;
+    ?>
+    <main>
+        <h1>Reajustador de Preços</h1>
+        <script>
+            function atualizarPorcentagem(val) {
+            document.getElementById('valor').innerHTML = val;
+            }
+        </script>
+        <form action="" method="get">
+            <label for="preco">Preço do Produto (R$)</label>
+            <input type="number" name="preco" id="preco" step="0.01">
+
+            <label for="reajuste">Qual será o percentual de reajuste? (<strong><span id="valor"><?= $reajuste ?></span>%</strong>)</label>
+            <input type="range" name="reajuste" id="reajuste" min="0" max="100" value="<?= $reajuste ?>" oninput="atualizarPorcentagem(this.value)">
+
+            <input type="submit" value="Reajustar">
+        </form>
+    </main>
+    <section>
+        <h2>Resultado do Reajuste</h2>
+        <?php 
+            $porcentagem = $preco * $reajuste / 100;
+            $novo_preco = $preco + $porcentagem;
+
+            echo "O produto que custava " . number_format($preco, 2, ",", ".") . ", com <strong>" . $reajuste . "% de aumento</strong> vai passar a custar <strong>R$" . number_format($novo_preco, 2, ",", ".") . "</strong> a partir de agora.";
+
+        ?>
+        
+    </section>
+</body>
+</html>
---
+++ desafios/desafio012/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP 012</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    $segundo = (int) ($_GET["segundo"] ?? 0);
+    ?>
+    <main>
+        <h1>Calculadora de Tempo</h1>
+            <form action="" method="get">
+            <label for="segundo">Qual o total de segundos</label>
+            <input type="number" name="segundo" id="segundo">
+
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+    <section>
+        <h2>Totalizando Tudo</h2>
+        <?php
+        $semana = floor($segundo / 604800);
+        $resto = $segundo % 604800;
+
+        $dia = floor($resto / 86400) ; // 86.400;
+        $resto = $resto % 86400;
+        
+        $hora = floor($resto / 3600);
+        $resto = $resto % 3600;
+        
+        $minuto = floor($resto / 60);
+        
+        $segundo_saida = $resto % 60;
+
+        echo "<p>Analizando o valor que você digitou, <strong>" . number_format($segundo, 0, ".", ".") . "</strong> segundos equivalem a um total de:</p>";
+
+        echo "<ul>
+                <li>" . number_format($semana, 0, ".", ".") . " semanas</li>
+                <li>" . number_format($dia, 0, ".", ".") . " dias</li>
+                <li>" . number_format($hora, 0, ".", ".") . " horas</li>
+                <li>" . number_format($minuto, 0, ".", ".") . " minutos</li>
+                <li>" . number_format($segundo_saida, 0, ".", ".") . " segundos</li>
+            </ul>";
+        ?>
+    </section>
+</body>
+</html>
